f2cec3895baf7c5f948437d1201c4e26284a8762
Bumbilo <dimagolyak@gmail.com>
2018-01-10 16:38:15 +0200

    some change in structure server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express     = require('express');
+const bodyParser  = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-
-const app = express();
-
-var db = null;
-
+const db          = require('./db');
+const app         = express();
+const port = 3040;
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({extended: true}));
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is work on port ${port}`);
+  });
+
+});
+
+
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -31,15 +43,16 @@ app.get('/users', (req, res, next) => {
   })
 });
 
-MongoClient.connect('mongodb://home_accounting:M4766b@ds247317.mlab.com:47317/home_accounting', (err, database) => {
-  if(err) {
-    console.log('ERRORSSS =>', err);
-  }
-
-  db = database;
-
-  app.listen(3030, () => {
-    console.log('Server is work !!!');
-  });
+app.listen(port, () => {
+  console.log(`Server is work on port ${port}`);
 });
+
+// MongoClient.connect('mongodb://home_accounting:M4766b@ds247317.mlab.com:47317/home_accounting', (err, database) => {
+//   if(err) {
+//     console.log('ERRORSSS =>', err);
+//   }
+//
+//   db = database;
+//
+// });
 
