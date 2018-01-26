093316efcb414ba1563a578cb7b271ca3eadec8e
Bumbilo <dimagolyak@gmail.com>
2018-01-26 13:32:17 +0200

    added structure for system module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { UserService } from "./shared/services/user.service";
-import { AuthService } from "./shared/services/auth.service";
+import { UserService } from './shared/services/user.service';
+import { AuthService } from './shared/services/auth.service';
+import { SystemModule } from './system/system.module';
 
 
 @NgModule({
@@ -19,9 +19,11 @@ import { AuthService } from "./shared/services/auth.service";
     BrowserModule,
     HttpModule,
     AuthModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SystemModule
   ],
   providers: [UserService, AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
