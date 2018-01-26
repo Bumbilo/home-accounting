ea2e85cad82756acf9f7842fd816580c7ff7ccc7
Bumbilo <dimagolyak@gmail.com>
2018-01-26 13:11:50 +0200

    added dinamic validation and started with system

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -12,33 +12,43 @@ import { User } from '../../shared/models/user.model';
 })
 export class RegistrationComponent implements OnInit {
 
-   form: FormGroup
+  form: FormGroup;
 
-  constructor(
-     private userService: UserService,
-     private router: Router,
-  ) { }
+  constructor(private userService: UserService,
+              private router: Router,) {
+  }
 
   ngOnInit() {
-     this.form = new FormGroup({
-        'email': new FormControl(null, [Validators.required, Validators.email]),
-        'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
-        'name': new FormControl(null, [Validators.required]),
-        'agree': new FormControl(false, [Validators.requiredTrue]),
-     })
+    this.form = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      'name': new FormControl(null, [Validators.required]),
+      'agree': new FormControl(false, [Validators.requiredTrue]),
+    });
   }
 
   onSubmit() {
-    const { email, password, name } = this.form.value;
+    const {email, password, name} = this.form.value;
     const user = new User(email, password, name);
 
-    console.log(user);
     this.userService.createNewUser(user)
-        .subscribe((user: User) => {
-          this.router.navigate(['/login'], {
-            queryParams: { nowCanLogin: true }
-          });
+      .subscribe((user: User) => {
+        this.router.navigate(['/login'], {
+          queryParams: {nowCanLogin: true}
         });
+      });
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.userService.getUserByEmail(control.value).subscribe((user: User) => {
+        if (user) {
+          resolve({forbiddenEmail: true});
+        } else {
+          resolve(null);
+        }
+      });
+    });
   }
 
 }
