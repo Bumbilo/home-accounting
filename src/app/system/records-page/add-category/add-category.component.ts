39e7d762096c4d6a8309a6a1b532b228e676ba68
Bumbilo <dimagolyak@gmail.com>
2018-02-09 16:56:25 +0200

    add

--- src/app/system/records-page/add-category/add-category.component.ts
+++ src/app/system/records-page/add-category/add-category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-category',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    let {name, capacity} = form.value;
+    if (capacity < 0) capacity *= -1;
   }
 
 }
---
+++ src/app/system/shared/models/category.model.ts
@@ -0,0 +1,7 @@
+export class Category {
+  costructor(public name: string,
+             public capacity: number,
+             id?: number) {
+
+  }
+}
